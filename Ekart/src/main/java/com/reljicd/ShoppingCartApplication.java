9776a2c56b254cbf70da19cba6795aefbdbaf5cd
Pathmesh <141057304+Pathmesh@users.noreply.github.com>
2024-11-20 22:49:00 +0530

    Update ShoppingCartApplication.java

--- Ekart/src/main/java/com/reljicd/ShoppingCartApplication.java
+++ Ekart/src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -3,7 +3,7 @@ package com.reljicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
